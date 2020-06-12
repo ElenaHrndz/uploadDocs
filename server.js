5a564cbfa9ca5c218b7f45569805f88c8c3a9981
ElenaHrndz <hrndzelena@gmail.com>
2020-06-11 20:42:23 -0500

    upload using html

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const compression = require('compression');
 const app = express();
+var fs = require('fs');
 
 const path = require('path');
 const multer = require('multer');
+
+
+app.use(compression())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let storage = multer.diskStorage({
     destination:(req, file, cb) => {
@@ -16,14 +23,13 @@ let storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('userFile'), (req, res) => {
+    console.log(req.file);
+    upload.single(req.file);
     console.log(`Storage location ${req.hostname}/${req.file.path}`);
     return res.send(req.file)
 });
